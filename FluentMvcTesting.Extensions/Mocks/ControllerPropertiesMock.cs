03fd578e2107f76a24e0b5d0b0b440aa856db8fe
Agustin Camino <agustin.camino@gmail.com>
2017-01-10 17:39:33 -0500

    Here be dragons

---
+++ FluentMvcTesting.Extensions/Mocks/ControllerPropertiesMock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+
+namespace FluentMvcTesting.Extensions.Mocks
+{
+    public class ControllerPropertiesMock
+    {
+        private const string OriginHeader = "Origin";
+        private readonly Mock<ControllerContext> _controllerContext;
+
+        public ControllerPropertiesMock(string defaultUrl = "http://www.example.com")
+        {
+            var mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.SetupGet(r => r.Url).Returns(new Uri(defaultUrl));
+            mockRequest.Setup(r => r.Headers).Returns(new NameValueCollection {{OriginHeader, defaultUrl}});
+
+            var mockResponse = Mock.Of<HttpResponseBase>();
+
+            _controllerContext = new Mock<ControllerContext>();
+            _controllerContext.Setup(x => x.HttpContext.Request).Returns(mockRequest.Object);
+            _controllerContext.Setup(x => x.HttpContext.Response).Returns(mockResponse);
+            _controllerContext
+                .Setup(x => x.HttpContext.Response.ApplyAppPathModifier(It.IsAny<string>()))
+                .Returns((string s) => s);
+        }
+
+        /// <summary>
+        /// The controller context
+        /// </summary>
+        public ControllerContext ControllerContext => _controllerContext.Object;
+
+        /// <summary>
+        /// The URL helper
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// Url = mockControllerProperties.Url(RouteConfig.RegisterRoutes)
+        /// </code>
+        /// </example>
+        /// <param name="registerRoutes">Action for registering routes</param>
+        /// <returns>The URL helper</returns>
+        public UrlHelper Url(Action<RouteCollection> registerRoutes)
+        {
+            var routes = new RouteCollection();
+            registerRoutes(routes);
+
+            return new UrlHelper(
+                new RequestContext(_controllerContext.Object.HttpContext, new RouteData()),
+                routes);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# FluentMVCTesting.Extensions
+
+This library provides XML extensions for fluent unit testing of ASP.NET MVC Controllers.
+
+## How to use it
+
+```csharp
+[Test]
+public void ShouldRespondWithXml()
+{
+    var controller = new CallController();
+    controller
+        .WithCallTo(c => c.Connect("555-5555"))
+        .ShouldReturnXmlResult(data =>
+        {
+            StringAssert.Contains(
+                "Thanks for contacting us", data.XPathSelectElement("Response/Say").Value);
+            Assert.That(data.XPathSelectElement("Response/Dial").Value, Is.EqualTo("555-5555"));
+        });
+}
+```
